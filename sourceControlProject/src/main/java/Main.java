407bc9c9f4745707a79703959df75c543f70330f
Adonis Bodea <31371688+adonisbodea02@users.noreply.github.com>
2019-11-21 10:45:54 +0200

    new new new commit

--- sourceControlProject/src/main/java/Main.java
+++ sourceControlProject/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    private static final String version = "4443cc9e25";
-    private int l = 12;
+    private static final String version = "da72fc3171";
+    private int l = 1234;
     public static void main(String[] args) {
         System.out.println("version: " + version);
     }
