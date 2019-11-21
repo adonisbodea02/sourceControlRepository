4a5291157f77a8120b0519904c78b3f81f10eff0
Adonis Bodea <31371688+adonisbodea02@users.noreply.github.com>
2019-11-21 10:31:39 +0200

    fourth commit

--- sourceControlProject/src/main/java/Main.java
+++ sourceControlProject/src/main/java/Main.java
@@ -1,11 +1,7 @@
-import java.io.*;
-import java.net.URL;
-
 public class Main {
     private static final String version = "0529cf374e";
-    private static final String gui = "after";
+    private int l = 0;
     public static void main(String[] args) {
         System.out.println("version: " + version);
     }
-
 }
