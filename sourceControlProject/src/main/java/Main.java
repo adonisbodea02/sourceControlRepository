35233ba93593d3d6d4fa6ba93d67dd94d8815b4b
Adonis Bodea <31371688+adonisbodea02@users.noreply.github.com>
2019-11-21 10:28:31 +0200

    third commit

--- sourceControlProject/src/main/java/Main.java
+++ sourceControlProject/src/main/java/Main.java
@@ -2,9 +2,10 @@ import java.io.*;
 import java.net.URL;
 
 public class Main {
-    private static final String versionHash = "73214e8507";
+    private static final String version = "0529cf374e";
+    private static final String gui = "after";
     public static void main(String[] args) {
-        System.out.println("NEW " + versionHash);
+        System.out.println("version: " + version);
     }
 
 }
