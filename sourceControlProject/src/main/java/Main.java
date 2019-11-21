3eac219d82b34c6b275bbb5d86490f9f54c1e505
Adonis Bodea <31371688+adonisbodea02@users.noreply.github.com>
2019-11-21 10:34:50 +0200

    commit new branch

--- sourceControlProject/src/main/java/Main.java
+++ sourceControlProject/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     private static final String version = "0529cf374e";
-    private int l = 0;
+    private int l = 12;
     public static void main(String[] args) {
         System.out.println("version: " + version);
     }
